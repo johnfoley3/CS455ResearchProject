644aa9c87b79027e7d0a044e4155bd33159bd9bb
dpaulman <ap7632@truman.edu>
2015-04-19 11:00:08 -0500

    Added Utility Class and a function that initializes integer arrays so they are preset to zero.
    Just use this class to hold useful functions that reduce redundancy

---
+++ src/Utility.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Andrew
+ * Helpful static functions that reduce redundant code
+ */
+public class Utility
+{
+	public static void intitializeIntArrayToZero (int [] array)
+	{
+		for (int i = 0; i< array.length;i++)
+		{
+			array[i] = 0;
+		}
+	}
+}
